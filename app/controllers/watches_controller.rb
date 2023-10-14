df5fe0c3c26ebbec292510a11075b3a8c2ffc284
Joseph Lee <jhjlee702@gmail.com>
2023-10-14 14:05:57 -0600

    add gems and create watch controller and view

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -2,4 +2,8 @@ class WatchesController < ApplicationController
   def index
     @watches = Watch.all
   end
+
+  def show
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/watches', to: 'watches#index'
+  get '/watches/:id', to: 'watches#show'
 end
