bb63640347eeb75d8db9acd3e67f29631b51abfe
Joseph Lee <jhjlee702@gmail.com>
2023-10-15 23:15:46 -0600

    ignore this commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,19 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/', to: "welcome#index"
   get '/brands', to: 'brands#index'
-  get '/brands/new', to: 'brands#new'
+
+  get '/brands/new', to: 'brands#new_brand'
   # :id is being sent as a param[:id] to the show function in brands controller
-  post "/brands", to: "brands#create"
+  post "/brands", to: "brands#create_brand"
+
   get '/brands/:id', to: 'brands#show'
   get "/brands/:id/edit", to: "brands#edit"
   patch "/brands/:id", to: "brands#update"
+
   get '/brands/:id/watches', to: 'brands#brand_watches_index'
-  
+  get '/brands/:brand_id/watches/new', to: 'watches#new_watch'
+  post '/watches', to: 'watches#create_watch'
+
   get '/watches', to: 'watches#index'
   get '/watches/:id', to: 'watches#show'
 end
